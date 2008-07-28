5f0cc2f7f65f6e0467a8a00c50098c7edf57ea6e
simon.martinelli <simon.martinelli@144746d5-0153-0410-a480-d13fab3f5f40>
2008-07-28 14:13:03 +0000

    new initial load

---
+++ trunk/simject-factory/src/main/java/org/simject/exception/SimConfigException.java
@@ -0,0 +1,8 @@
+package org.simject.exception;
+
+public class SimConfigException extends SimException {
+
+	public SimConfigException(String message, Exception cause) {
+		super(message, cause);
+	}
+}
