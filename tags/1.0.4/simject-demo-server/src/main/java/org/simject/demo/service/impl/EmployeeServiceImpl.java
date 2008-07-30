9bbbb4254c4ebdec42077e37ca22b9d571824c0d
simon.martinelli <simon.martinelli@144746d5-0153-0410-a480-d13fab3f5f40>
2008-07-30 13:32:45 +0000

    [maven-release-plugin]  copy for tag 1.0.4

---
+++ tags/1.0.4/simject-demo-server/src/main/java/org/simject/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package org.simject.demo.service.impl;
+
+import javax.annotation.Resource;
+
+import org.simject.demo.business.EmployeeManager;
+import org.simject.demo.model.Employee;
+import org.simject.demo.service.EmployeeService;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Resource
+	private EmployeeManager employeeManager;
+
+	public String getHello(Employee employee) {
+		return this.employeeManager.getHello(employee);
+	}
+
+	public void find(String name, Integer age) {
+		
+	}
+}
